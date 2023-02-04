6054e1f819b595d9e739f42b9b5ec7fb70b88570
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-02-03 22:58:25 -0500

    Just getting started

---
+++ src/App.js
@@ -0,0 +1,40 @@
+
+const App = () => {
+  const categories = [
+    {
+      id:1,
+      title: "Hats"
+    },
+    {
+      id:2,
+      title: "Jackets"
+    },
+    {
+      id:3,
+      title: "Shoes"
+    },
+    {
+      id:4,
+      title: "Women"
+    },
+    {
+      id:5,
+      title: "Men"
+    },
+  ];
+  return (
+    <div className="categories-container">
+      {categories.map(({title}) => (
+        <div className="category-container">
+          <div className="background-image"/>
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
