4ec76a9e309f5ed989f4f45032de0672f5a92504
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-03-03 15:13:47 -0500

    creating components and moving code :rocket:

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
+import CategoryItem from './components/category-item/category-item.component';
+import './categories.styles.scss';
 
 const App = () => {
   const categories = [
     {
-      id:1,
-      title: "Hats"
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-      id:2,
-      title: "Jackets"
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-      id:3,
-      title: "Shoes"
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-      id:4,
-      title: "Women"
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      id:5,
-      title: "Men"
-    },
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
   ];
+  
   return (
     <div className="categories-container">
-      {categories.map(({title}) => (
-        <div className="category-container">
-          <div className="background-image"/>
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category}/>
       ))}
     </div>
   );
