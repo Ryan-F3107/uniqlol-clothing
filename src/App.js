7c0ffa1b465a2ed82586d67896a3819f9fa44dd0
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-04-08 19:13:38 -0400

    :rocket: built blueprint for navigation-bar with styling and routing in place

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import Home from "./routes/home/home.component";
-import {Routes,Route, Outlet} from 'react-router-dom';
+import {Routes,Route} from 'react-router-dom';
+import Navigation from "./routes/navigation/navigation.component";
 
-const Navigation = () =>{
+const Shop = () => {
   return(
-    <div>
-      <div>
-        <h1>Navigation</h1>
-      </div>
-      <Outlet/>
-    </div>
-  );
+    <div><h1>Shop Page</h1></div>
+  )
 };
+
 const App = () => {
   return(
     <Routes>
       <Route path="/" element={<Navigation/>}>
         <Route index element={<Home/>}/>
+        <Route path="/Shop" element={<Shop/>}/>
       </Route>
     </Routes>
   );
