39f25d35437a1df43a6184de088a580c3b8031b6
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-04-08 16:48:43 -0400

    :smile: starting building navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import Home from "./routes/home/home.component";
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route, Outlet} from 'react-router-dom';
 
+const Navigation = () =>{
+  return(
+    <div>
+      <div>
+        <h1>Navigation</h1>
+      </div>
+      <Outlet/>
+    </div>
+  );
+};
 const App = () => {
   return(
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<Navigation/>}>
+        <Route index element={<Home/>}/>
+      </Route>
     </Routes>
   );
 };
