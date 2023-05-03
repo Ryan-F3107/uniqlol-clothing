12fde856eeec5f70ed9787a032b371f4ee977ae8
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-05-03 19:47:36 -0400

    :rocket: added products into Shop page, worked on styling. ProductsContext added

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -9,7 +9,7 @@ export const ProductsContext = createContext({
  * We render the children between the context provider.
  * Provider value is what we want to store
 */
-export const ProductProvider = ({children}) => {
+export const ProductsProvider = ({children}) => {
     const [products, setProducts] = useState(PRODUCTS);
     const value = {products}// pass products as an object.
     return(
