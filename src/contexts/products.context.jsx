01ce72fdd1af9ffc2d79f15f8a484e23a3b21bdd
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-05-03 17:02:29 -0400

    Added product context :package:

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,14 +1,18 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import PRODUCTS from '../shop-data.json';
 
-export const ProductContext = createContext({});
+export const ProductsContext = createContext({
+    products: [],
+});
 
 /**
  * We render the children between the context provider.
  * Provider value is what we want to store
 */
 export const ProductProvider = ({children}) => {
+    const [products, setProducts] = useState(PRODUCTS);
+    const value = {products}// pass products as an object.
     return(
-        <ProductContext.Provider>{children}</ProductContext.Provider>
+        <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
     )
 }
