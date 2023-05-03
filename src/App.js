64e57b133685588e35afcbf746db4d6924e8c59a
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-05-02 20:23:56 -0400

    Working on Shop Page using JSON object

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import Home from "./routes/home/home.component";
 import {Routes,Route} from 'react-router-dom';
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-
-const Shop = () => {
-  return(
-    <div><h1>Shop Page</h1></div>
-  )
-};
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return(
