0e221c3ca1849871b3c0a973699b70fb14656d3e
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-05-10 17:11:35 -0400

    :package: working on checkout Page, implemented basics of checkout page

---
+++ src/routes/check-out/checkout.component.jsx
@@ -0,0 +1,11 @@
+import './checkout.styles.scss';
+
+const Checkout = () => {
+    return(
+        <div className='checkout-container'>
+            <h1>Checkout Page</h1>
+        </div>
+    )
+}
+
+export default Checkout;
