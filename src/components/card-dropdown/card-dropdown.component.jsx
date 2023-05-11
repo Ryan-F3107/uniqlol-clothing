9ff909e48a986e651501ba306e14125fc5a54f8e
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-05-11 15:40:16 -0400

    :package: working on checkout items page

--- src/components/card-dropdown/card-dropdown.component.jsx
+++ src/components/card-dropdown/card-dropdown.component.jsx
@@ -7,7 +7,6 @@ import { useNavigate } from 'react-router-dom';
 
 const CardDropdown = () =>{
     const {cartItems} = useContext(CartContext);
-
     const navigate = useNavigate();
     const goToCheckoutHandler = () => {
         navigate('/checkout')
--- src/routes/check-out/checkout.component.jsx
+++ src/routes/check-out/checkout.component.jsx
@@ -1,9 +1,27 @@
 import './checkout.styles.scss';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
 
 const Checkout = () => {
-    return(
+    const { cartItems, addItemToCart } = useContext(CartContext);
+    return (
         <div className='checkout-container'>
             <h1>Checkout Page</h1>
+            <div>
+                {
+                    cartItems.map((cartItem) => {
+                        const { id, name, quantity } = cartItem;
+                        return (
+                            <div className={id}>
+                                <h2>{name}</h2>
+                                <span>{quantity}</span>
+                                <span>decrement</span>
+                                <span onClick={()=> addItemToCart(cartItem)}>increment</span>
+                            </div>
+                        );
+                        })
+                }
+            </div>
         </div>
     )
 }
