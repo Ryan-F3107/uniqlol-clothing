cce3746650e6f0496a1a50f98180fc2d6db5dede
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-05-17 13:38:03 -0400

    :fire: joined JSON with Shop Page, working on components preview

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,28 +1,28 @@
 import { createContext, useState, useEffect } from "react";
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
-export const ProductsContext = createContext({
-    products: [],
+export const CategoriesContext = createContext({
+    categoriesMap: {},
 });
 
 /**
  * We render the children between the context provider.
  * Provider value is what we want to store
 */
-export const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState([]);
+export const CategoriesProvider = ({ children }) => {
+    const [categoriesMap, setCategoriesMap] = useState({});
     //fire off only once, to input items into firestore - Hats,Shoes,sneakers -removed the useEffect as we wanted to do it once only -- typically not done on front-end
     //Next useEffect function: ---
     //Since below useEffect contains a function that uses async await, we need to create an async func -- getCategoriesMap to invoke getCategoriesAndDocuments() func..
     useEffect(() =>{
         const getCategoriesMap = async () =>  {
             const categoryMap = await getCategoriesAndDocuments();
-            console.log(categoryMap);
-        }
+            setCategoriesMap(categoryMap);
+        };
         getCategoriesMap();
     },[])
-    const value = { products }// pass products as an object.
+    const value = { categoriesMap }// pass products as an object.
     return (
-        <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
+        <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
     )
 }
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,27 @@
-import { useContext } from "react";
-import { ProductsContext } from "../../contexts/products.context";
+import { useContext, Fragment } from "react";
+import { CategoriesContext } from "../../contexts/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
 import './shop.styles.scss'
 
 const Shop = () => {
-    const {products} = useContext(ProductsContext);
+    const { categoriesMap } = useContext(CategoriesContext);
+    console.log("cM: ",categoriesMap);
     return (
-        <div className="products-container">
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product}/>   //pass the whole product as a product prop
-            ))}
-        </div>
-    )
+        <Fragment>
+            {   //object.keys() returns an array of keys
+                Object.keys(categoriesMap).map((title) => (
+                    <Fragment key={title}>
+                        <h2>{title}</h2>
+                        <div className="products-container">
+                            {categoriesMap[title].map((product) => (
+                                <ProductCard key={product.id} product={product}/>   //pass the whole product as a product prop
+                            ))}
+                        </div>
+                    </Fragment>
+                ))
+            }
+        </Fragment>
+    );
 }
 
 export default Shop;
