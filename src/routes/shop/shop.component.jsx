45b46ccde44bab2d56ac350d8e84b5df31314af0
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-05-17 14:20:57 -0400

    :rocket: implemented Categories Preview on Shop Page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,26 +1,18 @@
-import { useContext, Fragment } from "react";
+import { useContext } from "react";
 import { CategoriesContext } from "../../contexts/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
 import './shop.styles.scss'
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 const Shop = () => {
     const { categoriesMap } = useContext(CategoriesContext);
     console.log("cM: ",categoriesMap);
     return (
-        <Fragment>
-            {   //object.keys() returns an array of keys
-                Object.keys(categoriesMap).map((title) => (
-                    <Fragment key={title}>
-                        <h2>{title}</h2>
-                        <div className="products-container">
-                            {categoriesMap[title].map((product) => (
-                                <ProductCard key={product.id} product={product}/>   //pass the whole product as a product prop
-                            ))}
-                        </div>
-                    </Fragment>
-                ))
-            }
-        </Fragment>
+        <div className="shop-container">
+            {Object.keys(categoriesMap).map((title) => {
+                const products = categoriesMap[title];
+                return (<CategoryPreview key={title} title={title} products={products}/>);
+            })}
+        </div>
     );
 }
 
