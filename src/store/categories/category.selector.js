21ae0620e8db50c5f060086870b292c87e47cad0
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-06-15 16:30:03 -0400

    :construction: working with REdux :/ HARD

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,19 @@
-export const selectCategoriesMap = (state) => 
-state.categories.categories
-.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
+import { createSelector } from "reselect";  //used for memoization ~ caching - creates memoized selector
+
+const selectCategoryReducer = (state) => state.categories;
+
+//1st param - takes an array, the output of that, is used in second paramater
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories//this runs only if the selectCategoryReducer value (categories) is different
+);
+//implement memoization here as well. as long as categories does not change, do not run below code after []  
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => 
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
   }, {})
+);
