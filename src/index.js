7a2b92766dc5df4d142f311c2e43f6c097161de5
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-06-22 22:34:31 -0400

    added redux persists

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-          <App/>
-      </BrowserRouter>
+      <PersistGate loading = {null} persistor={persistor}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,18 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
 import logger from "redux-logger";
-
+import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
+const persistConfig = {
+    key:'root',
+    storage,
+    blacklist: ['user']//reducers we don't want to persist.It won't persist the user logged in as that is done elsewhere
+}
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 //helper, runs everytime, before dispatch hits a reducer.Before it hits the action it hits the middleware
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter( Boolean);// if we are in the production node env, it will result in a false and the logger won't run.
 const composedEnhancers = compose(applyMiddleware(...middleWares));
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
