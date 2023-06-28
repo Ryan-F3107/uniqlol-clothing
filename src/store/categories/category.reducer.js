907d46d538041fdf25ef1cfdfa21367c827b3aa7
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-06-28 16:04:29 -0400

    :construction: implementing Redux-Thunk

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -2,13 +2,19 @@ import { CATEGORIES_ACTION_TYPE } from "./category.types";
 
 export const CATEGORIES_INTIAL_STATE = {
     categories: [],
-}
+    isLoading: false,
+    error: null,
+};
 
 export const categoriesReducer = (state = CATEGORIES_INTIAL_STATE, action = {} ) => {
     const {type, payload} = action;
     switch(type){
-        case CATEGORIES_ACTION_TYPE.SET_CATEGORIES:
-            return {...state, categories: payload};
+        case CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START: //when we begin the api request
+            return {...state, isLoading: true};
+        case CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS:
+            return {...state, categories: payload, isLoading: false};
+        case CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED:
+            return {...state, error: payload ,isLoading: false}
         default:
             return state;
     }
--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,8 @@ import { persistStore, persistReducer } from "redux-persist";
 import logger from "redux-logger";
 import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
+import thunk from "redux-thunk";
+
 const persistConfig = {
     key:'root',
     storage,
@@ -11,7 +13,9 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 //helper, runs everytime, before dispatch hits a reducer.Before it hits the action it hits the middleware
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter( Boolean);// if we are in the production node env, it will result in a false and the logger won't run.
+const middleWares = [
+    process.env.NODE_ENV !== 'production' && logger, thunk
+].filter( Boolean);// if we are in the production node env, it will result in a false and the logger won't run.
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
 
