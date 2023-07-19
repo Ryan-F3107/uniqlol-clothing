706e83e96bd2f15d4b8d2b8e9e5e7f66369d81c7
Ryan-F3107 <ryanfernandes3107@gmail.com>
2023-07-19 11:52:12 -0400

    Implemented stripe api

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -9,11 +9,33 @@ const PaymentForm= () => {
         if(!stripe || !elements){
             return;
         }
-        
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ amount: 10000}),
+        }).then((res) => res.json());
+        const clientSecret = response.paymentIntent.client_secret;
+        const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Ryan Test'
+                }
+            }
+        });
+        if(paymentResult.error){
+            alert(paymentResult.error)
+        } else {
+            if(paymentResult.paymentIntent.status === "succeeded"){
+                alert("payment successful");
+            }
+        }
     }
     return(
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit Card Payment:</h2>
                 <CardElement/>
                 <Button buttonType={BUTTON_TYPE_CLASS.inverted}>Pay Now</Button>
